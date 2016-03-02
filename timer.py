30e1ddd0be0c850b6b62ac86b42da4807648821d
fenrir <en@eller.no>
2016-03-02 07:45:52 +0100

    changed default setting

--- timer.py
+++ timer.py
@@ -3,16 +3,19 @@
 from time import sleep
 import os
 
-def timer(time=50):
+def timer(time=0):
     for i in range(0,time):
         sleep(1)
         print(i)
     os.system("mpv "+str(os.getcwd())+"/bell.wav")
 
+default = 50
 while True:
-    time = input("time in seconds (default 50 s): ")
+    time = input("time in seconds (default {} s): ".format(default))
     if not time:
-        time = 50
+        time = default
     else:
         time = int(time)
+
     timer(time=time)
+    default = time
